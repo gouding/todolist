58340a9800ee451bbcebfcf0481be972cc9de28a
denzel <422293027@qq.com>
2018-02-07 18:31:39 +0800

    develop

--- server/routes/index.js
+++ server/routes/index.js
@@ -12,6 +12,35 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Denzel' });
 });
 
+//查询 
+router.get('/query', (req, res, next) => {
+  var word = req.query.word;
+  var _filter = {
+    $or: [  //多字段同时模糊查询
+      { name: { $regex: word, $options: '$i' } }, //$options:'$i' 不区分大小写
+      { content: { $regex: word, $options: '$i' } }
+    ]
+  };
+  UserModel.find(_filter)
+    .sort({ '_id': -1 })
+    .exec((err, doc) => {
+      if (err) {
+        res.send({
+          status: -1,
+          msg: err
+        });
+      } else {
+        res.send({
+          status: 1,
+          msg: '',
+          result: doc
+        });
+      }
+    });
+});
+
+
+
 router.get('/login', (req, res, next) => {
   UserModel.find({}, (err, docs) => {
     if (err) {
@@ -37,13 +66,13 @@ router.post('/addUser', (req, res, next) => {
     });
   }
   var content = {
-    name: user
+    name: user,
+    uid: Math.floor(Math.random() * 100)
   }
-  var model = new UserModel(content);
-  model.name = 'aaa';
-  model.uid = 20;
-  console.log('model',model,typeof model);
+  var model = new UserModel(content, false);
+  console.log('model', model, typeof model);
   model.save((err) => {
+    console.log(err);
     if (err) {
       console.log(err);
       res.send({
